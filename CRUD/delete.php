6af4294cd0db8996f99664da27755c3251057c48
myngabie <yasmyn.machado@etec.sp.gov.br>
2022-04-12 10:48:24 -0300

    Adicionada a função delete

---
+++ CRUD/delete.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Source\Models\User;
+use Source\Models\Address;
+
+$user = (new User())->findById(1);
+if($user){
+    $address = (new Address)->find("user_id ={$user->id}")->fetch(false);
+    $address->destroy();
+    $user->destroy();
+} else{
+    var_dump($user);
+}
+
+?>
